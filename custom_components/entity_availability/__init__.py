e7cdd7e95299eba722f2cf26686dede5e81a3559
Italo Lombardi <italo.lombardi@sap.com>
2026-05-04 21:54:15 +0200

    fix: add http to after_dependencies and CONFIG_SCHEMA for hassfest
    - Add http to after_dependencies (required for StaticPathConfig import)
    - Add CONFIG_SCHEMA = cv.config_entry_only_config_schema for async_setup

--- custom_components/entity_availability/__init__.py
+++ custom_components/entity_availability/__init__.py
@@ -11,12 +11,15 @@ from homeassistant.components.http import StaticPathConfig
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN
 from .coordinator import EntityAvailabilityCoordinator
 from .services import async_setup_services, async_unload_services
 
 _LOGGER = logging.getLogger(__name__)
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.BINARY_SENSOR]
 
